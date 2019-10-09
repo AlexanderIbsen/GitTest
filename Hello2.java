92418702f3ef7ed89d7a1b65e721c6d68d142815
AlexanderIbsen <56348126+AlexanderIbsen@users.noreply.github.com>
2019-10-09 14:28:57 +0200

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,8 @@
+public class Hello2 {
+	
+	public static void main (String[] args){
+		//generate some simple output
+		System.out.println("Hello Wolrd!"); // first line 
+		System.out.println("How are you?"); // another line
+	}
+}
